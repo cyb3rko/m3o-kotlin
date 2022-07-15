815c0a510a662038f0beaa0904f69dfd1c0ac18d
Niko Diamadis <niko@cyb3rko.de>
2022-07-15 11:17:23 +0200

    Add new Bitcoin balance function

--- src/main/kotlin/com/cyb3rko/m3okotlin/data/Bitcoin.kt
+++ src/main/kotlin/com/cyb3rko/m3okotlin/data/Bitcoin.kt
@@ -3,7 +3,13 @@ package com.cyb3rko.m3okotlin.data
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class BitcoinRequest(val symbol: String)
+internal data class BitcoinBalanceRequest(val address: String)
 
 @Serializable
-data class BitcoinResponse(val price: Float, val symbol: String)
+data class BitcoinBalanceResponse(val balance: String)
+
+@Serializable
+internal data class BitcoinPriceRequest(val symbol: String)
+
+@Serializable
+data class BitcoinPriceResponse(val price: Float, val symbol: String)
--- src/main/kotlin/com/cyb3rko/m3okotlin/services/BitcoinService.kt
+++ src/main/kotlin/com/cyb3rko/m3okotlin/services/BitcoinService.kt
@@ -1,8 +1,10 @@
 package com.cyb3rko.m3okotlin.services
 
 import com.cyb3rko.m3okotlin.M3O
-import com.cyb3rko.m3okotlin.data.BitcoinRequest
-import com.cyb3rko.m3okotlin.data.BitcoinResponse
+import com.cyb3rko.m3okotlin.data.BitcoinBalanceRequest
+import com.cyb3rko.m3okotlin.data.BitcoinBalanceResponse
+import com.cyb3rko.m3okotlin.data.BitcoinPriceRequest
+import com.cyb3rko.m3okotlin.data.BitcoinPriceResponse
 import io.ktor.client.request.*
 
 private const val SERVICE = "bitcoin"
@@ -17,12 +19,22 @@ private const val SERVICE = "bitcoin"
 object BitcoinService {
 
     /**
+     * Get the BTC balance of an address
+     * @since 0.2.2
+     */
+    suspend fun balance(address: String): BitcoinBalanceResponse {
+        return M3O.ktorHttpClient.post(M3O.getUrl(SERVICE, "Balance")) {
+            body = BitcoinBalanceRequest(address)
+        }
+    }
+
+    /**
      * Get the price of bitcoin
      * @since 0.1.0
      */
-    suspend fun price(symbol: String = "BTCUSD"): BitcoinResponse {
+    suspend fun price(symbol: String = "BTCUSD"): BitcoinPriceResponse {
         return M3O.ktorHttpClient.post(M3O.getUrl(SERVICE, "Price")) {
-            body = BitcoinRequest(symbol)
+            body = BitcoinPriceRequest(symbol)
         }
     }
 }
