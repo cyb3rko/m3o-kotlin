a22d67d65d19c5f6586ea03a367f23055f3c6478
Niko Diamadis <niko@cyb3rko.de>
2022-07-22 14:57:38 +0200

    Update Stream method descriptions

--- src/main/kotlin/com/cyb3rko/m3okotlin/services/StreamService.kt
+++ src/main/kotlin/com/cyb3rko/m3okotlin/services/StreamService.kt
@@ -10,19 +10,16 @@ private const val SERVICE = "stream"
 /**
  * **Ephemeral message streams**
  *
- * The stream service provides ephemeral message streams for building chat
- * applications, feeds and timelines. Simply send messages to a channel and know
- * they'll expire after 24 hours. Streams are limited to 1000 messages and 1000
- * streams in total. Max message size is 512 characters.
+ * The stream service provides ephemeral message streams. Messages expire after
+ * 24 hours. Streams are limited to 1000 messages and 1000 streams in total. Max
+ * message size is 512 characters.
  *
  * @since 0.1.0
  */
 object StreamService {
 
     /**
-     * Create a channel with a given name and description. Channels are created
-     * automatically but this allows you to specify a description that's
-     * persisted for the lifetime of the channel.
+     * Create a channel by name
      * @since 0.1.0
      */
     suspend fun createChannel(description: String, name: String) {
